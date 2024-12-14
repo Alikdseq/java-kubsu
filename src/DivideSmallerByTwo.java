f55a068376d4f0a46087311fb694e82a7a3c002e
Alikdseq <alikhan2102@mail.ru>
2024-12-14 14:48:01 +0300

    init repo

---
+++ src/DivideSmallerByTwo.java
@@ -0,0 +1,14 @@
+public class DivideSmallerByTwo {
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            int x = Integer.parseInt(args[0]);
+            int y = Integer.parseInt(args[1]);
+
+            int smaller = Math.min(x, y);
+            double result = smaller / 2.0;
+            System.out.println("Результат: " + result);
+        } else {
+            System.out.println("Неверное количество параметров");
+        }
+    }
+}
