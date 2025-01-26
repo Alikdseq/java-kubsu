edac15a47c1de13e93737b2a8039d9d0c2f71304
Alikdseq <130677803+Alikdseq@users.noreply.github.com>
2025-01-26 11:54:50 +0300

    Update Main.java

--- src/task_4/zad1/Main.java
+++ src/task_4/zad1/Main.java
@@ -2,16 +2,20 @@ package task_4.zad1;
 
 public class Main {
     public static void main(String[] args) {
+        // Создаем животных
         Dog dog = new Dog("Buddy", 3);
         Cat cat = new Cat("Whiskers", 2);
         Bird bird = new Bird("Tweety", 1);
 
+        // Создаем зоопарк с вместимостью 5 животных
         Zoo zoo = new Zoo(5);
 
+        // Добавляем животных в зоопарк
         zoo.addAnimal(dog);
         zoo.addAnimal(cat);
         zoo.addAnimal(bird);
 
+        // Выводим звуки всех животных в зоопарке
         System.out.println("Звуки животных в зоопарке:");
         zoo.makeAllSounds();
     }
